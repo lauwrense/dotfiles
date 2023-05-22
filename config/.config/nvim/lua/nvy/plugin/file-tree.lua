fcad7a91d417c9e26cb27b25e3a41450b25c6e13
lauwrense <tenwasalaw@gmail.com>
2023-05-22 21:47:09 +0800

    fix: nvim-tree ignores diffview tab

--- config/.config/nvim/lua/nvy/plugin/file-tree.lua
+++ config/.config/nvim/lua/nvy/plugin/file-tree.lua
@@ -178,7 +178,7 @@ return {
                 sync = {
                     open = true,
                     close = true,
-                    ignore = {},
+                    ignore = { "DiffviewFiles" },
                 },
             },
             notify = {
--- config/.config/nvim/lua/nvy/plugin/git.lua
+++ config/.config/nvim/lua/nvy/plugin/git.lua
@@ -1,7 +1,7 @@
 return {
     { "lewis6991/gitsigns.nvim", config = true },
     { "sindrets/diffview.nvim",  config = true },
-    { "TimUntersberger/neogit",  config = true },
+    { "TimUntersberger/neogit", config =true },
     {
         "pwntester/octo.nvim",
         cmd = "Octo",
