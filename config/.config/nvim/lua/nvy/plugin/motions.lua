a9d3aea6d623e253ddf80f80cb78b8cb2c15112d
lauwrense <tenwasalaw@gmail.com>
2023-05-26 18:52:24 +0800

    feat(nvim): added and configured motion plugins

--- config/.config/nvim/lua/nvy/plugin/motions.lua
+++ config/.config/nvim/lua/nvy/plugin/motions.lua
@@ -1,8 +1,31 @@
 return {
     {
+        "m4xshen/hardtime.nvim",
+        config = function()
+            require("hardtime").setup({
+                hint = true,
+                disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "NeogitStatus" }
+            })
+        end
+    },
+    {
         "ggandor/leap.nvim",
         config = function()
-            require('leap').add_default_mappings(true)
+            local leap = require("leap")
+            leap.opts = {
+                special_keys = {
+                    repeat_search = '<tab>',
+                    next_phase_one_target = '<tab>',
+                    next_target = { '<tab>', ';' },
+                    prev_target = { '<S-tab>', ',' },
+                    next_group = '<tab>',
+                    prev_group = '<S-tab>',
+                    multi_accept = '<enter>',
+                    multi_revert = '<backspace>',
+                }
+            }
+
+            leap.add_default_mappings(true)
         end
     },
     {
@@ -11,4 +34,15 @@ return {
             require("flit").setup()
         end
     },
+    { "ggandor/leap-spooky.nvim", config = true },
+
+    {
+        "kylechui/nvim-surround",
+
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup()
+        end
+    }
 }
