4ccea4db00882097383283f3db92c51748995d3f
lauwrense <tenwasalaw@gmail.com>
2023-06-01 14:54:57 +0800

    feat(nvim): Keymaps added
    Added kemaps for telescope: grep string, live grep, help tags and man pages; and Neogit.
    Added labeled_modes configuration for flit.nvim
    Fix for leap configuration not registering
    Removed <nop> binding for arrow keys

--- config/.config/nvim/lua/nvy/config/keymaps.lua
+++ config/.config/nvim/lua/nvy/config/keymaps.lua
@@ -2,10 +2,6 @@ vim.keymap.set("", "<Space>", "<Nop>", { silent = true, remap = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>", { silent = true, remap = true })
-vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>", { silent = true, remap = true })
-vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>", { silent = true, remap = true })
-vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>", { silent = true, remap = true })
 
 
 local Telescope = {}
@@ -24,5 +20,25 @@ Telescope.find_files = function()
     })
 end
 
-vim.keymap.set("n", "<leader>gu", Neogit.open, { remap = true, silent = true })
-vim.keymap.set("n", "<leader>ff", Telescope.find_files, { remap = true })
+Telescope.grep_string = function()
+    require("telescope.builtin").grep_string()
+end
+
+Telescope.live_grep = function()
+    require("telescope.builtin").live_grep()
+end
+
+Telescope.help_tags = function()
+    require("telescope.builtin").help_tags()
+end
+
+Telescope.man_pages = function()
+    require("telescope.builtin").man_pages()
+end
+
+vim.keymap.set("n", "<leader>gu", Neogit.open, { remap = true, silent = true, desc = "Open Neogit" })
+vim.keymap.set("n", "<leader>ff", Telescope.find_files, { remap = true, desc = "Find Files" })
+vim.keymap.set("n", "<leader>fs", Telescope.grep_string, { remap = true, desc = "Grep String" })
+vim.keymap.set("n", "<leader>fg", Telescope.live_grep, { remap = true, desc = "Live Grep" })
+vim.keymap.set("n", "<leader>fh", Telescope.help_tags, { remap = true, desc = "Help Tags" })
+vim.keymap.set("n", "<leader>fm", Telescope.man_pages, { remap = true, desc = "Man Pages" })
--- config/.config/nvim/lua/nvy/plugin/motions.lua
+++ config/.config/nvim/lua/nvy/plugin/motions.lua
@@ -12,29 +12,39 @@ return {
         "ggandor/leap.nvim",
         config = function()
             local leap = require("leap")
-            leap.opts = {
-                special_keys = {
-                    repeat_search = '<tab>',
-                    next_phase_one_target = '<tab>',
-                    next_target = { '<tab>', ';' },
-                    prev_target = { '<S-tab>', ',' },
-                    next_group = '<tab>',
-                    prev_group = '<S-tab>',
-                    multi_accept = '<enter>',
-                    multi_revert = '<backspace>',
-                }
-            }
-
             leap.add_default_mappings(true)
+            require("leap").opts.special_keys = {
+                repeat_search = '<tab>',
+                next_phase_one_target = '<tab>',
+                next_target = { '<tab>', ';' },
+                prev_target = { '<S-tab>', ',' },
+                next_group = '<space>',
+                prev_group = '<S-tab>',
+                multi_accept = '<enter>',
+                multi_revert = '<backspace>',
+            }
         end
     },
     {
         "ggandor/flit.nvim",
         config = function()
-            require("flit").setup()
+            require("flit").setup({
+                labeled_modes = "vno",
+            })
         end
     },
-    { "ggandor/leap-spooky.nvim", config = true },
+    {
+        "ggandor/leap-spooky.nvim",
+        config = function()
+            require("leap-spooky").setup({
+                affixes = {
+                    magnetic = { window = 'm', cross_window = 'M' },
+                    remote = { window = 'r', cross_window = 'R' },
+                },
+                paste_on_remote_yank = false,
+            })
+        end
+    },
 
     {
         "kylechui/nvim-surround",
