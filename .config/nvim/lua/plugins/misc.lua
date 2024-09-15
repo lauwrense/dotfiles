fb4c5f4f705935633e1568b47b87be1b09270acf
lauwrense <lauwrenceteneza@gmail.com>
2024-09-15 11:45:34 +0800

    fix(nvim): moved noice.nvim into ui category

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -27,49 +27,4 @@ return {
             })
         end
     },
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            ---@module "noice.nvim"
-            cmdline = {
-                enabled = false
-            },
-            messages = {
-                enabled = false,
-            },
-            popupmenu = {
-                enabled = false,
-            },
-            lsp = {
-                progress = {
-                    enabled = false,
-                },
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-                hover = {
-                    opts = {
-                        size = {
-                            max_height = 20,
-                            max_width = 80,
-                        },
-                    },
-                },
-                signature = {
-                    opts = {
-                        size = {
-                            max_height = 20,
-                            max_width = 80,
-                        },
-                    },
-                },
-            },
-        },
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-        }
-    }
 }
