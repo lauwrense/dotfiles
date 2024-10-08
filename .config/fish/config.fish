7566e8b5b7a84611575b052d3a42223e4b9fc7e9
lauwrense <lauwrenceteneza@gmail.com>
2024-10-08 12:32:31 +0800

    feat(config): merged bat, fish, tmux, and starship config

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+
+if status is-interactive
+    fish_vi_key_bindings
+
+    bind --erase -a
+
+    if test -z "$NVIM"; and not set -q TMUX
+        cd ~;
+    end
+end
+
+set os (uname)
+set fish_color_valid_path
+set -g fish_autosuggestion_enabled 0
+
+abbr -a ls eza -la --icons --git -s type
+abbr -a ll eza -la --icons --git --git-ignore -s type -T
+
+zoxide init fish | source
+starship init fish | source
+fzf --fish | source
+
+fish_config theme choose Catppuccin\ Frappe
+fish_add_path /home/lauw/.cargo/bin
+
+# Theme
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#babbf1,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
+--color=selected-bg:#51576d \
+--multi"
