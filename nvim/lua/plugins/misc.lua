5e96808243de183d44f404008b460c6cd6d29c9a
lauwrense <lauwrenceteneza@gmail.com>
2024-10-16 14:05:51 +0800

    feat(nvim): removed `]t` and `[t`, added window resize keymaps

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -8,20 +8,11 @@ return {
 
             local wk = require("which-key")
 
-
             wk.add({
                 { "<leader>f", group = "+fzf" },
                 { "<leader>t", group = "+term" },
                 { "<leader>l", group = "+list" },
                 { "<leader>v", group = "+virt text" },
-                {
-                    { "]", group = "+next" },
-                    { "]a", "<cmd>tabnext<cr>", desc = "Next tab" },
-                },
-                {
-                    { "[", group = "+prev" },
-                    { "[a", "<cmd>tabprev<cr>", desc = "Prev tab" },
-                },
             })
         end,
     },
---
+++ nvim/plugin/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<M-/>", "<c-w>5>")
+vim.keymap.set("n", "<M-m>", "<c-w>5<")
+vim.keymap.set("n", "<M-,>", "<c-w>+")
+vim.keymap.set("n", "<M-.>", "<c-w>-")
