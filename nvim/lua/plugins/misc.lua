df821c51632011496f57b71638a04ddf2655130f
lauwrense <lauwrenceteneza@gmail.com>
2024-10-16 13:46:35 +0800

    feat(nvim): added diagnostic keymap

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -8,9 +8,13 @@ return {
 
             local wk = require("which-key")
 
+            local diagnostic_vt = true
+
             wk.add({
                 { "<leader>f", group = "+fzf" },
+                { "<leader>t", group = "+term" },
                 { "<leader>l", group = "+list" },
+                { "<leader>v", group = "+virt text" },
                 {
                     { "]", group = "+next" },
                     { "]a", "<cmd>tabnext<cr>", desc = "Next tab" },
@@ -18,6 +22,21 @@ return {
                 {
                     { "[", group = "+prev" },
                     { "[a", "<cmd>tabprev<cr>", desc = "Prev tab" },
+                },
+                {
+                    {
+                        "<leader>vd",
+                        function()
+                            diagnostic_vt = not diagnostic_vt
+                            vim.diagnostic.config({
+                                virtual_text = diagnostic_vt,
+                            })
+                        end,
+                        desc = function()
+                            return "diagnostic "
+                                .. (diagnostic_vt and "(on)" or "(off)")
+                        end,
+                    },
                 },
             })
         end,
