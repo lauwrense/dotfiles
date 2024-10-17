2232e7f10b6e0f780752f79b081145f3054d4e12
lauwrense <lauwrenceteneza@gmail.com>
2024-10-17 17:25:16 +0800

    perf(nvim/diagnostic): make lazy not load which-key

--- nvim/plugin/diagnostic.lua
+++ nvim/plugin/diagnostic.lua
@@ -1,14 +1,14 @@
-local diagnostic_vt = true
+local diagnostic_vt = false
 
-require("which-key").add({
-    "<leader>vd",
-    function()
-        diagnostic_vt = not diagnostic_vt
-        vim.diagnostic.config({
-            virtual_text = diagnostic_vt,
-        })
-    end,
-    desc = function()
-        return "diagnostic " .. (diagnostic_vt and "(on)" or "(off)")
-    end,
-})
+local function set_diagnostic_keymap()
+    diagnostic_vt = not diagnostic_vt
+    vim.diagnostic.config({
+        virtual_text = diagnostic_vt,
+    })
+    vim.keymap.set("n", "<leader>vd", set_diagnostic_keymap, {
+        desc = "diagnostic " .. (diagnostic_vt and "(on)" or "(off)"),
+        remap = true,
+    })
+end
+
+set_diagnostic_keymap()
