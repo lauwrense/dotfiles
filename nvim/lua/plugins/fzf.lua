c057a8cc1879c7917352a12920901415308eb1ee
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-01-17 22:14:26 +0800

    feat(nvim): replaced telescope with fzf

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,80 @@
+return {
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        keys = {
+            {
+                "<leader>ff",
+                function()
+                    require("fzf-lua").files()
+                end,
+                desc = "Find Files",
+            },
+            {
+                "<leader>fs",
+                function()
+                    require("fzf-lua").grep()
+                end,
+                desc = "Grep String",
+            },
+            {
+                "<leader>fg",
+                function()
+                    require("fzf-lua").live_grep()
+                end,
+                desc = "Live Grep",
+            },
+            {
+
+                "<leader>fh",
+                function()
+                    require("fzf-lua").helptags()
+                end,
+                desc = "Help Tags",
+            },
+
+            {
+                "<leader>fm",
+                function()
+                    require("fzf-lua").manpages()
+                end,
+                desc = "Man Pages",
+            },
+            {
+                "<leader>fB",
+                "<cmd>FzfLua tabs previewer=builtin<cr>",
+                desc = "Tabs",
+            },
+            { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "Buffers" },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("Colorscheme", {
+                group = vim.api.nvim_create_augroup("RefresgFzfColors", {}),
+                callback = function()
+                    vim.api.nvim_set_hl(0, "FzfLuaBorder", {
+                        fg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg,
+                        bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg,
+                    })
+                end,
+            })
+        end,
+        config = function()
+            require("fzf-lua").setup({
+                fzf_colors = true,
+                files = {
+                    cwd_prompt = false,
+                },
+                keymap = {
+                    builtin = {
+                        ["<c-f>"] = "preview-page-down",
+                        ["<c-b>"] = "preview-page-up",
+                    },
+                    fzf = {
+                        ["ctrl-f"] = "preview-page-down",
+                        ["ctrl-b"] = "preview-page-up",
+                    },
+                },
+            })
+        end,
+    },
+}
