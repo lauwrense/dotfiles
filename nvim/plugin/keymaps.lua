4f452d9491e8e6de67dc91fe68d5832c11e3db43
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-03-17 02:46:46 +0800

    feat(nvim): changed default hover size to by either colorcolumn or 80

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -7,3 +7,13 @@ vim.keymap.set("n", "]q", "<cmd>cnext<cr>")
 vim.keymap.set("n", "[q", "<cmd>cprev<cr>")
 
 vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "K", function()
+    local size = vim.wo.colorcolumn or 80
+
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+        max_height = size,
+        max_width = size,
+    })
+
+    vim.lsp.buf.hover()
+end)
