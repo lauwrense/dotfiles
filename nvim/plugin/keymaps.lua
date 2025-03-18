e514b13f99872dd1df5f798b880e742a276f3a6d
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-03-18 21:51:47 +0800

    feat(nvim): added lsp keybinds

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -10,10 +10,22 @@ vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
 vim.keymap.set("n", "K", function()
     local size = vim.wo.colorcolumn or 80
 
-    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-        max_height = size,
-        max_width = size,
-    })
+    vim.lsp.handlers["textDocument/hover"] =
+        vim.lsp.with(vim.lsp.handlers.hover, {
+            max_height = size,
+            max_width = size,
+        })
 
     vim.lsp.buf.hover()
 end)
+
+if vim.version.lt(vim.version(), "0.11") then
+    vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename)
+    vim.keymap.set("n", "<leader>lca", vim.lsp.buf.code_action)
+    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references)
+    vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation)
+    vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition)
+    vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition)
+else
+    vim.print("EDIT KEYMAPS")
+end
