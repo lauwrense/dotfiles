e2ad5409785c19ecc8c5ffe97cac18f9617dc737
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-03-20 17:59:00 +0800

    style(nvim): change style of plugin/qf.lua

--- nvim/plugin/qf.lua
+++ nvim/plugin/qf.lua
@@ -1,24 +1,11 @@
-local group = vim.api.nvim_create_augroup("QuickFixGroup", {})
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-    group = group,
-    pattern = "quickfix",
-    callback = function()
-
-        vim.opt_local.colorcolumn = ""
-        vim.opt_local.signcolumn = "no"
-        vim.opt_local.number = false
-        vim.opt_local.relativenumber = false
-
-        vim.keymap.set("n", "q", "<cmd>q<cr>")
-    end,
-})
-
+-- FUNCTIONS
 local function copen_no_focus()
     local win = vim.api.nvim_get_current_win()
     vim.cmd("copen")
     vim.api.nvim_set_current_win(win)
 end
 
+-- KEYBINDS
 vim.keymap.set("n", "]q", function()
     if vim.fn.getqflist({ size = 1 }).size < 1 then
         vim.notify("Empty Quickfix", vim.log.levels.INFO)
@@ -38,3 +25,16 @@ vim.keymap.set("n", "[q", function()
     copen_no_focus()
     vim.cmd("cprev")
 end, { desc = "Next on quickfix list" })
+
+-- AUTOGROUPS
+local qf_group = vim.api.nvim_create_augroup("QuickFixGroup", {})
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+    group = qf_group,
+    pattern = "quickfix",
+    callback = function()
+        vim.opt_local.colorcolumn = ""
+        vim.opt_local.signcolumn = "no"
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+    end,
+})
