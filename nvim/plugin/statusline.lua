123135e77bed658fd9e10d95619110a6d927aab3
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-03-27 15:30:04 +0800

    style(nvim): used the actual fix for the issue rather than a hack

--- nvim/plugin/statusline.lua
+++ nvim/plugin/statusline.lua
@@ -6,12 +6,4 @@ vim.api.nvim_create_autocmd("Colorscheme", {
 })
 
 vim.o.statusline = "%{%v:lua.require'custom.statusline'.render()%}"
-
--- HACK: this solves issue describe at neovim/neovim#27731
-vim.api.nvim_create_autocmd("Filetype", {
-    pattern = "qf",
-    group = vim.api.nvim_create_augroup("SetStatusQuickFix", {}),
-    callback = function()
-        vim.o.statusline = "%{%v:lua.require'custom.statusline'.render()%}"
-    end,
-})
+vim.g.qf_disable_statusline = true
