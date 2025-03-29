f436c75f99b292e9db1cbdd8cb70bb88af7037fc
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-03-30 01:42:10 +0800

    refactor(nvim): extract keymap "<leader>vd" in function set_diagnostic_keymap

--- nvim/plugin/diagnostic.lua
+++ nvim/plugin/diagnostic.lua
@@ -5,10 +5,9 @@ local function set_diagnostic_keymap()
     vim.diagnostic.config({
         virtual_text = diagnostic_vt,
     })
-    vim.keymap.set("n", "<leader>vd", set_diagnostic_keymap, {
-        desc = "diagnostic " .. (diagnostic_vt and "(on)" or "(off)"),
-        remap = true,
-    })
 end
 
-set_diagnostic_keymap()
+vim.keymap.set("n", "<leader>vd", set_diagnostic_keymap, {
+    desc = "diagnostic " .. (diagnostic_vt and "(on)" or "(off)"),
+    remap = true,
+})
