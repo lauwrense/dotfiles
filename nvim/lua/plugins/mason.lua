b3f8c83fd31ccc8443b69a2176053f2a8cf4672e
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-04-07 23:18:28 +0800

    feat(nvim): mason.nvim added

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        cmd = {
+            "Mason",
+            "MasonInstall",
+            "MasonUninstall",
+            "MasonUninstallAll",
+            "MasonLog",
+            "MasonUpdate",
+        },
+
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        },
+    },
+}
