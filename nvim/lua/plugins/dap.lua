131f7d3c3e447638bd9f68c34846b423ff33bb7a
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-04-07 23:20:09 +0800

    feat(nvim): config for llvm-dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,14 +1,53 @@
 return {
     {
         "mfussenegger/nvim-dap",
-        lazy = true,
         dependencies = {
             "nvim-neotest/nvim-nio",
             "rcarriga/nvim-dap-ui",
             "theHamsta/nvim-dap-virtual-text",
         },
         config = function()
+            local dap = require("dap")
 
+            local lldb_dap = vim.fn.exepath("lldb-dap")
+            if lldb_dap ~= "" then
+                dap.adapters.lldb = {
+                    type = "executable",
+                    name = "lldb",
+                    command = lldb_dap,
+                }
+
+                dap.configurations.cpp = {
+                    {
+                        name = "Launch",
+                        type = "lldb",
+                        request = "launch",
+                        program = function()
+                            return vim.fn.input(
+                                "Path to executable: ",
+                                vim.fn.getcwd() .. "/",
+                                "file"
+                            )
+                        end,
+                        cwd = "${workspaceFolder}",
+                        stopOnEntry = false,
+                        args = {},
+                    },
+                }
+
+                dap.configurations.c = dap.configurations.cpp
+                dap.configurations.zig = {
+                    {
+                        name = "Launch",
+                        type = "lldb",
+                        request = "launch",
+                        program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
+                        cwd = "${workspaceFolder}",
+                        stopOnEntry = false,
+                        args = {},
+                    },
+                }
+            end
         end,
     },
 }
