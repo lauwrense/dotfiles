ea48854cc7e22b9ec4fc0cdeb57053f100bfc199
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-06-04 22:27:15 +0800

    feat(nvim): improved zig dap config, it now runs zig build

--- nvim/after/ftplugin/zig.lua
+++ nvim/after/ftplugin/zig.lua
@@ -1,15 +1,2 @@
 vim.g.zig_fmt_autosave = 0
 vim.opt_local.colorcolumn = "100"
-
-local dap = require("dap")
-dap.configurations.zig = {
-    {
-        name = "Launch",
-        type = "lldb",
-        request = "launch",
-        program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
-    },
-}
--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -7,7 +7,7 @@ return {
             "theHamsta/nvim-dap-virtual-text",
             "leoluz/nvim-dap-go",
         },
-        ft = { "c", "cpp" },
+        ft = { "c", "cpp", "zig" },
         config = function()
             local dap = require("dap")
             local dapui = require("dapui")
@@ -26,11 +26,13 @@ return {
                         type = "lldb",
                         request = "launch",
                         program = function()
-                            return vim.fn.input(
-                                "Path to executable: ",
-                                vim.fn.getcwd() .. "/",
-                                "file"
-                            )
+                            local path = vim.fn.input({
+                                prompt = "Path to executable: ",
+                                default = vim.fn.getcwd() .. "/",
+                                completion = "file",
+                            })
+
+                            return (path and path ~= "") and path or dap.ABORT
                         end,
                         cwd = "${workspaceFolder}",
                         stopOnEntry = false,
@@ -39,6 +41,32 @@ return {
                 }
 
                 dap.configurations.c = dap.configurations.cpp
+                dap.configurations.zig = {
+                    {
+                        name = "Launch",
+                        type = "lldb",
+                        request = "launch",
+                        program = function()
+                            local path = vim.fn.getcwd()
+                                .. "/zig-out/bin/"
+                                .. vim.fs.basename(vim.fn.getcwd())
+                            if not vim.uv.fs_stat(path) then
+                                vim.ui.input(
+                                    { prompt = "zig build " },
+                                    function(args)
+                                        vim.cmd("make " .. args)
+                                    end
+                                )
+                            end
+
+                            return not vim.uv.fs_stat(path) and dap.ABORT
+                                or path
+                        end,
+                        cwd = "${workspaceFolder}",
+                        stopOnEntry = false,
+                        args = {},
+                    },
+                }
             end
 
             vim.keymap.set("n", "<F1>", function()
@@ -53,6 +81,9 @@ return {
             end)
             vim.keymap.set("n", "<F4>", function()
                 dap.step_out()
+            end)
+            vim.keymap.set("n", "<F5>", function()
+                dapui.toggle()
             end)
             vim.keymap.set("n", "<M-b>", function()
                 dap.toggle_breakpoint()
