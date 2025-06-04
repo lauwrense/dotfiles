2981c17b4a5aa3b191301c484f5ad18876514c11
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-06-05 00:24:44 +0800

    fix(nvim): fixed treesitter emitting errors

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,20 +1,25 @@
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
 --- Highlight
-local treesitter_group = vim.api.nvim_create_augroup("user.treesitter.start", {})
+local treesitter_group =
+    vim.api.nvim_create_augroup("user.treesitter.start", {})
 vim.api.nvim_create_autocmd("FileType", {
     group = treesitter_group,
     callback = function(args)
         local filetype = vim.bo[args.buf].filetype
-        local lang = vim.treesitter.language.get_lang(filetype)
 
-        if not lang then
+        local ok, lang = pcall(vim.treesitter.language.get_lang, filetype)
+
+        if not ok or not lang then
             return
+        elseif package.loaded["nvim-treesitter"] then
+            require("nvim-treesitter").install(lang)
         end
 
-        require("nvim-treesitter").install(lang)
-
-        if lang and vim.treesitter.language.add(lang) then
+        if
+            lang
+            and vim.treesitter.language.add(lang)
+        then
             vim.treesitter.start()
         end
     end,
