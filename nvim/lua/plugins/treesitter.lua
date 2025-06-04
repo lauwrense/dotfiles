3b94d885271a3d3b10064b6a948f6dddd68d42b9
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-06-05 00:26:33 +0800

    fix(nvim): nvim-treesitter should not be lazy loaded

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
         name = "treesitter",
         branch = "main",
         build = ":TSUpdate",
-        lazy = false,
         config = function()
             require("nvim-treesitter").install({
                 "zig",
