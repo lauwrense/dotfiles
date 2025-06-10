6ba089d122137dc31ad41430293f1a0e670be350
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-06-11 00:48:58 +0800

    style: made treesitter file more readable

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -50,25 +50,24 @@ return {
         init = function()
             -- Highlight
             local treesitter_group =
-                vim.api.nvim_create_augroup("user.treesitter.start", {})
+                vim.api.nvim_create_augroup("user.treesitter", {})
+            local ts = require("nvim-treesitter")
 
             vim.api.nvim_create_autocmd("FileType", {
                 group = treesitter_group,
                 callback = function(args)
-                    local ft = vim.bo[args.buf].filetype
+                    local ft = args.match
                     local lang = vim.treesitter.language.get_lang(ft)
-                    local avalable = require("nvim-treesitter").get_available()
+                    local avalable = ts.get_available()
 
                     if vim.tbl_contains(avalable, lang) then
-                        require("nvim-treesitter")
-                            .install(lang)
-                            :await(function()
-                                vim.bo.indentexpr =
-                                    "v:lua.require'nvim-treesitter'.indentexpr()"
-                                vim.wo.foldexpr = vim.wo.foldexpr
-                                    or "v:lua.vim.treesitter.foldexpr()"
-                                vim.treesitter.start()
-                            end)
+                        ts.install(lang):await(function()
+                            vim.bo.indentexpr =
+                                "v:lua.require'nvim-treesitter'.indentexpr()"
+                            vim.wo.foldexpr = vim.wo.foldexpr
+                                or "v:lua.vim.treesitter.foldexpr()"
+                            vim.treesitter.start()
+                        end)
                     end
                 end,
             })
