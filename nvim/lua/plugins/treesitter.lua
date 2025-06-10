bc05943de42c5bd8740815747f7963b5d87e154a
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-06-10 22:46:05 +0800

    feat(nvim): moved treesitter.install to build function

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,45 +1,51 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
         name = "treesitter",
         branch = "main",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter").install({
-                "zig",
+        build = {
+            function()
+                local parser_installed = {
+                    "zig",
 
-                -- Go
-                "go",
-                "gosum",
-                "gomod",
-                "gowork",
+                    -- Go
+                    "go",
+                    "gosum",
+                    "gomod",
+                    "gowork",
 
-                -- Scripting
-                "lua",
-                "luap",
-                "luadoc",
+                    -- Scripting
+                    "lua",
+                    "luap",
+                    "luadoc",
 
-                "python",
+                    "python",
 
-                -- C
-                "doxygen",
-                "c",
-                "cpp",
-                "make",
-                "cmake",
+                    -- C
+                    "doxygen",
+                    "c",
+                    "cpp",
+                    "make",
+                    "cmake",
 
-                -- MD
-                "markdown",
-                "markdown_inline",
+                    -- MD
+                    "markdown",
+                    "markdown_inline",
 
-                -- Git
-                "diff",
-                "gitcommit",
-                "gitattributes",
-                "gitignore",
-                "git_rebase",
-                "git_config",
-            })
-        end,
+                    -- Git
+                    "diff",
+                    "gitcommit",
+                    "gitattributes",
+                    "gitignore",
+                    "git_rebase",
+                    "git_config",
+                }
+
+                require("nvim-treesitter").install(parser_installed)
+                require("nvim-treesitter").update()
+            end,
+            ":TSUpdate",
+        },
     },
 }
