14433762ab27e25dd6c2129bdb016542c7d17dfb
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-06-15 15:58:28 +0800

    feat(nvim): loclist is now sorted

--- nvim/plugin/qf.lua
+++ nvim/plugin/qf.lua
@@ -11,25 +11,31 @@ vim.api.nvim_create_autocmd({ "BufRead" }, {
     end,
 })
 
+local function cmp_item(a, b)
+    local fname = {
+        a = vim.api.nvim_buf_get_name(a.bufnr),
+        b = vim.api.nvim_buf_get_name(b.bufnr),
+    }
+
+    if a.bufnr == b.bufnr then
+        return (a.lnum == b.lnum) and (a.col < b.col) or (a.lnum < b.lnum)
+    else
+        return #fname.a < #fname.b
+    end
+end
+
 vim.api.nvim_create_autocmd({ "QuickFixCmdPost" }, {
     group = qf_group,
     callback = function()
-        local qflist = vim.fn.getqflist()
-        table.sort(qflist, function(a, b)
-            local fname = {
-                a = vim.api.nvim_buf_get_name(a.bufnr),
-                b = vim.api.nvim_buf_get_name(b.bufnr),
-            }
+        -- TODO: seperate this
+        local loclist = vim.fn.getloclist(0)
+        table.sort(loclist, cmp_item)
+        vim.fn.setloclist(0, loclist, "r")
 
-            if a.bufnr == b.bufnr then
-                return (a.lnum == b.lnum) and (a.col < b.col) or
-                     (a.lnum < b.lnum)
-            else
-                return #fname.a < #fname.b
-            end
-        end)
+        local qflist = vim.fn.getqflist()
+        table.sort(qflist, cmp_item)
         vim.fn.setqflist(qflist, "r")
     end,
 })
 
-vim.o.quickfixtextfunc = "v:lua.require'user.qf'.quickfixtextfunc"
+vim.opt.quickfixtextfunc = "v:lua.require'user.qf'.quickfixtextfunc"
