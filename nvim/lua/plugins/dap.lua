f1f4d0eb279b99288afb2972b9f744d4518bfea3
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-08-31 00:22:16 +0800

    refactor: move dap config for c

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -10,37 +10,6 @@ return {
             local dap = require("dap")
             local dapui = require("dapui")
 
-            local lldb_dap = vim.fn.exepath("lldb-dap") -- PERF: WSL is slow
-            if lldb_dap ~= "" then
-                dap.adapters.lldb = {
-                    type = "executable",
-                    name = "lldb",
-                    command = lldb_dap,
-                }
-
-                dap.configurations.cpp = {
-                    {
-                        name = "Launch",
-                        type = "lldb",
-                        request = "launch",
-                        program = function()
-                            local path = vim.fn.input({
-                                prompt = "Path to executable: ",
-                                default = vim.fn.getcwd() .. "/",
-                                completion = "file",
-                            })
-
-                            return (path and path ~= "") and path or dap.ABORT
-                        end,
-                        cwd = "${workspaceFolder}",
-                        stopOnEntry = false,
-                        args = {},
-                    },
-                }
-
-                dap.configurations.c = dap.configurations.cpp
-            end
-
             vim.keymap.set("n", "<F1>", function()
                 dap.continue()
             end)
