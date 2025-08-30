cdf67462691492f5459d348448aed77891c8c8d7
lauwrense <77498275+lauwrense@users.noreply.github.com>
2025-08-31 00:11:03 +0800

    feat(nvim): removed flash.nvim and oil.nvim

--- nvim/lua/plugins/motion.lua
+++ nvim/lua/plugins/motion.lua
@@ -1,47 +1,5 @@
 return {
     {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        opts = {
-            modes = {
-                search = {
-                    highlight = {
-                        backdrop = true,
-                    },
-                },
-                char = {
-                    jump_labels = true,
-                },
-            },
-        },
-        keys = {
-            {
-                "s",
-                mode = { "n", "x", "o" },
-                function()
-                    require("flash").jump()
-                end,
-                desc = "Flash",
-            },
-            {
-                "r",
-                mode = "o",
-                function()
-                    require("flash").remote()
-                end,
-                desc = "Remote Flash",
-            },
-            {
-                "<c-s>",
-                mode = { "c" },
-                function()
-                    require("flash").toggle()
-                end,
-                desc = "Toggle Flash Search",
-            },
-        },
-    },
-    {
         "kylechui/nvim-surround",
         version = "*",
         event = { "VeryLazy" },
