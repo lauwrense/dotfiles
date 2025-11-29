179d73cf97a1f068a786fdb6ea547c93907f5235
lauwrense <lauwrenceteneza@gmail.com>
2025-11-29 22:09:15 +0800

    chore: format unformatted code

--- nvim/after/ftplugin/zig.lua
+++ nvim/after/ftplugin/zig.lua
@@ -6,37 +6,37 @@ end
 
 local dap = require("dap")
 
-    dap.adapters.lldb = {
-        type = "executable",
-        name = "lldb",
-        command = vim.fn.expand("$MASON/bin/codelldb"),
-    }
+dap.adapters.lldb = {
+    type = "executable",
+    name = "lldb",
+    command = vim.fn.expand("$MASON/bin/codelldb"),
+}
 
-    dap.configurations.zig = {
-        {
-            name = "Launch",
-            type = "lldb",
-            request = "launch",
-            program = function()
-                local path = vim.fn.getcwd()
-                    .. "/zig-out/bin/"
-                    .. vim.fs.basename(vim.fn.getcwd())
+dap.configurations.zig = {
+    {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+            local path = vim.fn.getcwd()
+                .. "/zig-out/bin/"
+                .. vim.fs.basename(vim.fn.getcwd())
 
-                vim.ui.input({ prompt = "zig build " }, function(args)
-                    if tostring(args):find("^test") then
-                        path = vim.fs.find("test", {
-                            limit = 1,
-                            type = "file",
-                            path = "./.zig-cache",
-                        })[1]
-                    end
-                    vim.cmd.make(args)
-                end)
+            vim.ui.input({ prompt = "zig build " }, function(args)
+                if tostring(args):find("^test") then
+                    path = vim.fs.find("test", {
+                        limit = 1,
+                        type = "file",
+                        path = "./.zig-cache",
+                    })[1]
+                end
+                vim.cmd.make(args)
+            end)
 
-                return not vim.uv.fs_stat(path) and dap.ABORT or path
-            end,
-            cwd = "${workspaceFolder}",
-            stopOnEntry = false,
-            args = {},
-        },
-    }
+            return not vim.uv.fs_stat(path) and dap.ABORT or path
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+    },
+}
