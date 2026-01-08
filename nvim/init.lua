063c35cbbbbc183358c50ee1d4c19233ef629111
lauwrense <lauwrenceteneza@gmail.com>
2026-01-08 20:18:07 +0800

    nvim: add gitsigns.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,6 +44,7 @@ vim.pack.add({
     { src = "https://codeberg.org/mfussenegger/nvim-dap" },
     { src = "https://github.com/Jorenar/nvim-dap-disasm" },
     { src = "https://github.com/igorlfs/nvim-dap-view" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/Wansmer/treesj" },
     { src = "https://github.com/stevearc/oil.nvim" },
     {
